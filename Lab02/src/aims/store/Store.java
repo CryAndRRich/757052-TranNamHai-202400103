48909bb8ec36d8804f80dee9354d90c2a30e3880
CryAndRRich <willshane2006@gmail.com>
2025-10-23 14:27:55 +0700

    Implementation of the class Store 2:27pm 23/10/2025

--- Lab02/src/aims/store/Store.java
+++ Lab02/src/aims/store/Store.java
@@ -4,42 +4,52 @@ import java.util.ArrayList;
 import aims.disc.DigitalVideoDisc;
 
 public class Store {
-    public ArrayList<DigitalVideoDisc> items = new ArrayList<DigitalVideoDisc>();
-    private static int count = 0;
+    public ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
 
     public void addDVD(DigitalVideoDisc dvd) {
-        items.add(dvd);
-        count++;
+        itemsInStore.add(dvd);
+        System.out.println("Added DVD: " + dvd.getTitle() + " to the store");
     }
 
     public void removeDVD(DigitalVideoDisc dvd) {
         int j = 0;
-        for (int i = 0; i < count; i++) {
-            if (dvd.isMatch(items.get(i).getTitle())) {
+        for (int i = 0; i < itemsInStore.size(); i++) {
+            if (dvd.isMatch(itemsInStore.get(i).getTitle())) {
                 j = i;
                 break;
             }
         }
-        items.remove(j);
-        count--;
+        itemsInStore.remove(j);
     }
     
     public DigitalVideoDisc findDVD(String title) {
-        for (int i = 0; i < count; i++) {
-            if(items.get(i).isMatch(title)) {
-                return items.get(i);
+        for (int i = 0; i < itemsInStore.size(); i++) {
+            if(itemsInStore.get(i).isMatch(title)) {
+                return itemsInStore.get(i);
             }
         }
         return null;
     }
 
     public void getInfo() {
-        for(int i = 0; i < count; i++) {
-            System.out.println(items.get(i).toString());
+        for(int i = 0; i < itemsInStore.size(); i++) {
+            System.out.println(itemsInStore.get(i).toString());
         }
     }
 
     public int getNumberDVDs() {
-        return count;
+        return itemsInStore.size();
+    }
+
+    public void print() {
+        System.out.println("***********************STORE***********************");
+        if (itemsInStore.isEmpty()) {
+            System.out.println("The store is currently empty");
+        } else {
+            for (int i = 0; i < itemsInStore.size(); i++) {
+                System.out.println((i + 1) + ". " + itemsInStore.get(i).toString());
+            }
+        }
+        System.out.println("***************************************************");
     }
 }
